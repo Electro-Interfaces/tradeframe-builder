ca9a2af03d016d99b59e86a42d2f927ced260413
MagLook <162182428+MagLook@users.noreply.github.com>
2025-09-13 16:42:18 +0300

    cleanup: убрать лишние файлы деплоя, использовать GitHub Pages

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,19 +11,5 @@ export default {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G'
-  }, {
-    name: 'tradeframe-prod',
-    script: 'server.js',
-    cwd: '/home/user/webapp',
-    env: {
-      PORT: 8080,
-      HOST: '0.0.0.0',
-      NODE_ENV: 'production'
-    },
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '2G',
-    instances: 1,
-    exec_mode: 'cluster'
   }]
 }
